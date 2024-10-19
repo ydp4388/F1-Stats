0462c9de7bdd610abc72c97ea06fabdac4bf38d9
ydp4388 <ydp4388@rit.edu>
2024-10-19 13:02:29 +0530

    Parsing in Python and Formating in Javascript

--- server/f1_stats/live_data/tasks.py
+++ server/f1_stats/live_data/tasks.py
@@ -3,6 +3,7 @@ import time
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 import json
+import datetime
 
 channel_layer = get_channel_layer()
 
@@ -10,23 +11,39 @@ drivers = {1: "VER", 4: "NOR", 16: "LEC", 55: "SAI", 81: "PIA", 11: "PER", 63: "
 colors = {1: "rgb(51,51,255)", 4: "rgb(255,153,51)", 16: "rgb(255,0,0)", 55: "rgb(255,0,0)", 81: "rgb(255,153,51)", 11: "rgb(51,51,255)", 63: "rgb(160,160,160)", 44: "rgb(160,160,160)", 14: "rgb(0,102,0)", 18: "rgb(0,102,0)", 27: "rgb(255,204,204)", 22: "rgb(0,128,255)", 3: "rgb(0,128,255)", 38: "rgb(0,0,0)", 10: "rgb(0,0,0)", 20: "rgb(255,204,204)", 23: "rgb(0,76,153)", 31: "rgb(0,0,0)", 24: "rgb(0,255,0)", 2: "rgb(0,76,153)", 77: "rgb(0,255,0)"}
 
 def parseData(text: str) -> json:
-    retVal = {}
+    # Initialize the result dictionary
+    result = {
+        "drivers": {},
+        "timestamp": datetime.fromisoformat(data_list[2].rstrip('Z'))
+    }
+
     try:
-        text = text.replace('[', '').replace(']', '').split(',')
-        val = eval(text[1].strip())
-        if text[0].strip('\'') == "TimingAppData" and type(val) == dict:
-            print('here')
-            for i in val.get('Lines').keys():
-                dr_name = "UNK"
-                dr_color = 'rgb(0,0,0)'
-                if int(i) in drivers.keys():
-                    dr_name = drivers.get(int(i))
-                    dr_color = colors.get(int(i))
+        data_list = eval(text[1].strip())
+
+        lines_data = data_list[1]['Lines']
+
+
+        for driver_number, driver_data in lines_data.items():
+            driver_name = "UNK"
+            driver_color = 'rgb(0,0,0)'
+            if int(driver_number) in drivers.keys():
+                dr_name = drivers.get(int(driver_number))
+                dr_color = colors.get(int(driver_number))
+            
+            if 'Stints' in driver_data:
+                driver_info["stints"] = {}
+                for stint, stint_data in driver_data['Stints'].items():
+                    if 'LapTime' in stint_data:
+                        driver_info["stints"][stint] = {
+                            "lap_number": stint_data['LapNumber'],
+                            "lap_time": stint_data['LapTime']
+                        }
+            
+            result["drivers"][driver_number] = driver_info
                 
     except:
-        return retVal
-    # print(val)
-    return retVal
+        return result
+    return result
 
 @shared_task
 def add(x, y, channel):
