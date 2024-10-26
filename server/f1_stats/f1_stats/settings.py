1fdf82ec906b22f74979c831a6d13d5b83dacdbc
ydp4388 <ydp4388@rit.edu>
2024-10-26 14:38:47 +0530

    message debugging

--- server/f1_stats/f1_stats/settings.py
+++ server/f1_stats/f1_stats/settings.py
@@ -138,5 +138,5 @@ CHANNEL_LAYERS = {
 }
 
 # Celery settings
-CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_BROKER_URL = "redis://localhost:6379/0"
 CELERY_RESULT_BACKEND = "redis://localhost:6379"
